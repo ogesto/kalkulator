b91a2ca466a7a048a785b6b8f2de58ee4f640783
Tegsoo <kociembablazej@gmail.com>
2025-03-28 16:57:48 +0100

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -3,19 +3,17 @@ import math
 
 #1Podanie liczb i działania 
 print('Liczba 1 jest pierwsa z koleji więc: od niej zostanie coś odjęte, ona zostanie podzielona, i ona zostanie pomnożona przez liczbę 2')
+
+#znak
 znak = input('Podaj znak działania: ')
+
 #liczba 1
 a= float(input('Podaj liczbę 1: '))
+
 #liczba 2
 b = float(input('Podaj liczbę 2: '))
 
-#2Sprawdzenie błędów 
-if znak == '/' and a or b == 0 :
-    print('Nie dzielimy przez 0')
-    print(znak, a, b)
-    exit()
-
-#3Dalsze działania na liczbach 
+#Dalsze działania na liczbach 
 
 if znak == '+' :
     print('wynik to:',a + b)
@@ -26,16 +24,26 @@ elif znak == '-':
 elif znak == '*':
     print('wynik to:',a * b)
 
-elif znak == '/' and a or b != 0: 
-    print('wynik to:',a / b)
+elif znak == '/':
+    if  b == 0: 
+        print('Nie dzielimy przez 0')
+        print(a,znak, b)
+        exit()
+    else:
+        print('wynik to:',a / b)
 
 elif znak == '^':
-    print('potengi jeszcze nie działają')
+    odp = a ** b
+    print('wynik to:',odp)
 
-elif znak == '~':
-    print('pierwiastkowanie jeszcze nie działa ')
-    
+elif znak == '!':
+    odp = a ** (1/b)
+    print('wynik to:', odp)
     
 
-##Sprawdzenie działanie punktu 1 
+#Sprawdzenie działanie punktu 1 
 print(znak, a, b)
+
+print('koniec')
+
+exit()
