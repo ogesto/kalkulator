c9f926c10bb6404702b5ac1ba4db89f67505d889
Tegsoo <kociembablazej@gmail.com>
2025-04-05 22:20:52 +0200

    Update kalkulator.py
    My wersion

--- kalkulator.py
+++ kalkulator.py
@@ -1,49 +1,52 @@
 #inportowanie 
 import math
 
-#1Podanie liczb i działania 
+#Podanie liczb i działania 
 print('Liczba 1 jest pierwsa z koleji więc: od niej zostanie coś odjęte, ona zostanie podzielona, i ona zostanie pomnożona przez liczbę 2')
 
 #znak
 znak = input('Podaj znak działania: ')
 
 #liczba 1
-a= float(input('Podaj liczbę 1: '))
+a = float(input('Podaj liczbę 1: '))
 
 #liczba 2
 b = float(input('Podaj liczbę 2: '))
 
 #Dalsze działania na liczbach 
 
+#dodawanie
 if znak == '+' :
     print('wynik to:',a + b)
 
+#odejmowanie 
 elif znak == '-':
     print('wynik to:',a - b)
 
+#mnożenie
 elif znak == '*':
     print('wynik to:',a * b)
 
-elif znak == '/':
-    if  b == 0: 
+#dzielenie
+elif znak == '/': 
+    #Sprawdzenie błędów 
+    if b == 0 :
         print('Nie dzielimy przez 0')
-        print(a,znak, b)
-        exit()
+        print(znak, a, b)
+        exit()  
     else:
         print('wynik to:',a / b)
 
+#potęgowanie
 elif znak == '^':
-    odp = a ** b
-    print('wynik to:',odp)
+    print('wynik to:',a ** b)
 
+#pierwiastkowanie 
 elif znak == '!':
-    odp = a ** (1/b)
-    print('wynik to:', odp)
-    
+    print('wynik to:',a ** (1/b))  
 
-#Sprawdzenie działanie punktu 1 
-print(znak, a, b)
+    
+##Sprawdzenie przypisywania 
+print(a, znak, b)
 
 print('koniec')
-
-exit()
